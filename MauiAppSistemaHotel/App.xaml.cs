5a22435a2033c02bdd479b5f86ac2f73314e3cf6
geovanect <geovanecoutofacs@gmail.com>
2025-02-07 12:31:37 -0300

    Implementando a Logica e as condições do checkin e checkout

--- MauiAppSistemaHotel/App.xaml.cs
+++ MauiAppSistemaHotel/App.xaml.cs
@@ -1,8 +1,32 @@
 ﻿
+using MauiAppSistemaHotel.Models;
+
 namespace MauiAppSistemaHotel
 {
     public partial class App : Application
     {
+
+        public List<Quarto> lista_quartos = new List<Quarto>
+        {
+            new Quarto()
+            {
+                Descricao = "Suite Cobertura",
+                ValorDiariaAdulto = 550,
+                ValorDiariaCrianca = 180
+            },
+            new Quarto()
+            {
+                Descricao = "Suite com Varanda",
+                ValorDiariaAdulto = 270,
+                ValorDiariaCrianca = 90
+            },
+            new Quarto()
+            {
+                Descricao = "Suite Standart",
+                ValorDiariaAdulto = 190,
+                ValorDiariaCrianca = 40
+            }
+        };
         public App()
         {
             InitializeComponent();
