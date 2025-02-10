ed107af0e7d21ff4cd10be926a00a506bd371725
geovanect <geovanecoutofacs@gmail.com>
2025-02-10 16:47:19 -0300

    Finalização do layout e da logica no formulário.

---
+++ MauiAppSistemaHotel/Models/Hospedagem.cs
@@ -0,0 +1,31 @@
+﻿namespace MauiAppSistemaHotel.Models
+{
+    public class Hospedagem
+    {
+        public Quarto QuartoSelecionado { get; set; }
+
+        public int QuantAdultos { get; set; }
+        public int QuantCriancas { get; set; }
+        public DateTime DataCheckIn { get; set; }
+        public DateTime DataCheckOut { get; set; }
+        
+        public int Estadia {
+
+            get => DataCheckOut.Subtract(DataCheckIn).Days;
+        }
+
+        public double ValorTotal
+        {
+            get
+            {
+                double valor_adultos = QuantAdultos * QuartoSelecionado.ValorDiariaAdulto;
+                double valor_criancas = QuantCriancas * QuartoSelecionado.ValorDiariaCrianca;
+
+                double total = (valor_adultos + valor_criancas) * Estadia;
+
+                return total;
+            }
+        }
+
+    }
+}
--- MauiAppSistemaHotel/Views/ContratacaoHospedagem.xaml.cs
+++ MauiAppSistemaHotel/Views/ContratacaoHospedagem.xaml.cs
@@ -1,3 +1,5 @@
+using MauiAppSistemaHotel.Models;
+
 namespace MauiAppSistemaHotel.Views;
 
 public partial class ContratacaoHospedagem : ContentPage
@@ -16,30 +18,40 @@ public partial class ContratacaoHospedagem : ContentPage
         dtpck_checkin.MaximumDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month + 1, DateTime.Now.Day);
 
         
-		dtpck_checkout.MinimumDate = dtpck_checkin.Date.AddDays(1);
-		dtpck_checkout.MaximumDate = dtpck_checkin.Date.AddMonths(1);
+		dtpck_checkout.MinimumDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day +1 );
+        dtpck_checkout.MaximumDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month + 1, DateTime.Now.Day);
 
     }
 
-    private void Button_Clicked(object sender, EventArgs e)
+    private async void Button_Clicked(object sender, EventArgs e)
     {
 		try
 		{
-			Navigation.PushAsync(new HospedagemContratada());
+			Hospedagem h = new Hospedagem
+			{
+				QuartoSelecionado = (Quarto)pck_quarto.SelectedItem,
+				QuantAdultos = Convert.ToInt32(stp_adultos.Value),
+				QuantCriancas = Convert.ToInt32(stp_criancas.Value),
+				DataCheckIn = dtpck_checkin.Date,
+				DataCheckOut = dtpck_checkout.Date,
+			};
+
+			await Navigation.PushAsync(new HospedagemContratada()
+			{
+				BindingContext = h
+			});
 		}
 		catch (Exception ex) 
 		{
-			DisplayAlert("Ops", ex.Message, "OK");
+			await DisplayAlert("Ops", ex.Message, "OK");
 		}
     }
 
     private void dtpck_checkout_DateSelected(object sender, DateChangedEventArgs e)
     {
-		DatePicker elemento = sender as DatePicker;
+        DateTime data_selecionada_checkin = e.NewDate;
 
-		DateTime data_selecionada_checkin = elemento.Date;
-
-		dtpck_checkout.MinimumDate = data_selecionada_checkin.AddDays(1);
-		dtpck_checkout.MaximumDate = data_selecionada_checkin.AddMonths(6);
+        dtpck_checkout.MinimumDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day + 1);
+        dtpck_checkout.MaximumDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month + 1, DateTime.Now.Day);
     }
 }
